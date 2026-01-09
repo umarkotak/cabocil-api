c02af02be0f409f1be35c162e07b9d1c57054f94
umar <umarkotak@gmail.com>
2026-01-09 17:34:24 +0700

    add more worksheet

---
+++ db/seeds/000004_worksheet_2.sql
@@ -0,0 +1,46 @@
+INSERT INTO book_links
+  (group_name, name, url, image_url, premium)
+VALUES
+  -- Membaca Series
+  ('membaca dan menulis', 'membaca 2 suku kata', 'https://drive.google.com/file/d/1PjgIcNLoDoFcqd-iFKn-WsLXKhNz1PiD/view?usp=drive_link', '', true),
+  ('membaca dan menulis', 'membaca rangkaian 3 huruf', 'https://drive.google.com/file/d/1wieIrsV0mTxYCgrXHfZHi61t0d1hAwkl/view?usp=drive_link', '', true),
+
+  -- Seri Edukasi
+  ('seri edukasi', 'seri berhitung', 'https://drive.google.com/file/d/1ddTnc1g53O4qtU7iQ9PUkWApEbOOCZDd/view?usp=drive_link', '', true),
+  ('seri edukasi', 'seri alfabet', 'https://drive.google.com/file/d/1oBoGimYY8cKsxISJbvawNUhLw6_UHjhy/view?usp=drive_link', '', true),
+
+  -- Math Worksheets
+  ('math worksheets', '100 pages of beginning math', 'https://drive.google.com/file/d/1ArF1ugnbRHn8jtAqe4ESSFh7XN0T1qQU/view?usp=drive_link', '', true),
+  ('math worksheets', 'adding and subtracting worksheets 0-20', 'https://drive.google.com/file/d/1uZNQqyu5l07PlZATj6tFxMU-0Wb8vOyQ/view?usp=drive_link', '', true),
+  ('math worksheets', 'math fun workbook', 'https://drive.google.com/file/d/1V2vLy70CyGDH86LgDcjlH-U16UF-baTE/view?usp=drive_link', '', true),
+  ('math worksheets', 'math addition worksheets with solutions', 'https://drive.google.com/file/d/16zxesjX5adjioZvEbPLNWo3wEI_e_sws/view?usp=drive_link', '', true),
+  ('math worksheets', 'math subtraction worksheets with solutions', 'https://drive.google.com/file/d/1AZ4RbzX9p0Me06uPODXecV1-eAjkVHzD/view?usp=drive_link', '', true),
+  ('math worksheets', 'math workbook 1', 'https://drive.google.com/file/d/1ca_tU3PAjYbLQdIc2tEOKHyJh3PCYo8x/view?usp=drive_link', '', true),
+  ('math worksheets', 'math workbook 2', 'https://drive.google.com/file/d/11tUtVRHRUUdIwCu4-Rc7JazHn6Kw8FwO/view?usp=drive_link', '', true),
+
+  -- Maze Puzzles
+  ('maze puzzles', '50 rectangle mazes', 'https://drive.google.com/file/d/1kuTATNpXT_-Bftx_SqdAZVMOgngwm2WP/view?usp=drive_link', '', true),
+  ('maze puzzles', '50 summer mazes', 'https://drive.google.com/file/d/1W_uOCb6MlrCsQRpsDGY-wPWiVoy5aIe0/view?usp=drive_link', '', true),
+  ('maze puzzles', '75 mixed mazes', 'https://drive.google.com/file/d/17V0Zn6DU6gIJ49zSd5ynzz21pB-nZN4s/view?usp=drive_link', '', true),
+  ('maze puzzles', '100 maze worksheets', 'https://drive.google.com/file/d/1uqPw4Jo1h6msJIoLjBGA9NF52lFGXL6Q/view?usp=drive_link', '', true),
+  ('maze puzzles', '200 mega mazes', 'https://drive.google.com/file/d/1HRimOYjQVrbvZNMMOedZ7ZttzyLpUznZ/view?usp=drive_link', '', true),
+  ('maze puzzles', '200 mixed mazes', 'https://drive.google.com/file/d/1i0_ZsG--Q-mnFAbncQa5wURTxeolr2Ur/view?usp=drive_link', '', true),
+  ('maze puzzles', '300 circle mazes', 'https://drive.google.com/file/d/1PicYd9ZQSdsPvROata0BNofYhImu3AVO/view?usp=drive_link', '', true),
+  ('maze puzzles', '300 square mazes', 'https://drive.google.com/file/d/1eA9wzjxe3hZJAWOUshqMNWlxIgddX_W_/view?usp=drive_link', '', true),
+  ('maze puzzles', 'mega maze 5000 pages', 'https://drive.google.com/file/d/1sLLiiB9lw7iS6lGMhOUKuFA29sNNcQXe/view?usp=drive_link', '', true),
+
+  -- Mandala Coloring
+  ('mandala coloring', 'part 1 1000 mandala', 'https://drive.google.com/file/d/1vCoWSSp3zf8tv5QMY0yJ6P0kByUGb2G4/view?usp=drive_link', '', true),
+  ('mandala coloring', 'part 2 1000 mandala', 'https://drive.google.com/file/d/1Tqcv7Bfy-AG2xxhsupeXihHB49cxkGKc/view?usp=drive_link', '', true),
+  ('mandala coloring', 'part 3 part 1 1000 mandala', 'https://drive.google.com/file/d/1ePbOemoBnVqgpz6VGTUp1e_NrYxqOi_0/view?usp=drive_link', '', true),
+  ('mandala coloring', 'part 3 part 2 1000 mandala', 'https://drive.google.com/file/d/1lV6EHmTaSzgh8nbqenT2UHutr6Nv3cE6/view?usp=drive_link', '', true),
+  ('mandala coloring', 'part 4 1000 mandala', 'https://drive.google.com/file/d/1LORGPkgzttTgQPwoMaWLmyT5dexw-Yjx/view?usp=drive_link', '', true),
+  ('mandala coloring', 'part 5 1000 mandala', 'https://drive.google.com/file/d/1Ybzzz6jSeGZinE17yKm_s9gyBtSayeSX/view?usp=drive_link', '', true),
+  ('mandala coloring', 'part 6 1000 mandala', 'https://drive.google.com/file/d/1dwR9H2CMMkyUplKr-bLOPHPrHyCrJlci/view?usp=drive_link', '', true),
+
+  -- Busy Books
+  ('busy books', '19 monster busy book', 'https://drive.google.com/file/d/1jNh4yuTxOGv7wiLAZ8yF1CI78Dj0gakL/view?usp=drive_link', '', true),
+  ('busy books', '30+ space busy book', 'https://drive.google.com/file/d/16LIWLhveHc6yZKNCSlvXwpMNHRlP16d6/view?usp=drive_link', '', true),
+  ('busy books', '50+ busy book', 'https://drive.google.com/file/d/1aq7odBy0pPcbrM1IeJnYfHWDauZknGpa/view?usp=drive_link', '', true),
+  ('busy books', '125+ random busy book', 'https://drive.google.com/file/d/1vIqJMLmiUjml44IJufjsA-WTXxA9U-u1/view?usp=drive_link', '', true),
+  ('busy books', '350+ busy binder', 'https://drive.google.com/file/d/1gwo9t_ZPIIi47y6laN0uVCQsxA019_A4/view?usp=drive_link', '', true)
